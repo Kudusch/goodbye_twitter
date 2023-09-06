088f522329ba0cf7e7c46c5be775dee5482701cb
Tim Schatto-Eckrodt <kudusch@posteo.de>
2023-09-06 15:55:46 +0200

    Add entity parsing and text enrichment

--- js/main.js
+++ js/main.js
@@ -11,41 +11,97 @@ function document_ready(callback) {
     });
 }
 
+function parse_entities(tweet) {
+    console.log(tweet);
+    let raw_entities = [];
+    if ("entities" in tweet) {
+        Object.entries(tweet.entities).forEach(function(item) {
+            let key = item[0];
+            let entities = item[1];
+            if (entities.length != 0) {
+                entities.forEach(function(entity) {
+                    let parsed_entity = {
+                        "start":parseInt(entity.indices[0]),
+                        "end":parseInt(entity.indices[1]),
+                        "len":parseInt(entity.indices[1]) - parseInt(entity.indices[0])
+                    };
+                    if (key == "urls") {
+                        parsed_entity.replacement = "<a href='" + entity.expanded_url + "'>" + entity.display_url + "</a>";
+                    } else if (key == "hashtags") {
+                        parsed_entity.replacement = "<a href='?q=hashtag:" + entity.text + "'>" + entity.text + "</a>";
+                    } else if (key == "media") {
+                        parsed_entity.replacement = "<img src='data/tweets_media/" + tweet.id + "-" + entity.media_url.split("/").slice(-1) + "'>";
+                    } else if (key == "user_mentions") {
+                        parsed_entity.replacement = "<a href='https://twitter.com/" + entity.screen_name + "'>@" + entity.screen_name + "</a>";
+                    }
+                    raw_entities.push(parsed_entity);
+                });
+            }
+        });
+    }
+    
+    raw_entities.sort(function(a, b) {
+        return a.start-b.start;
+    });
+    return raw_entities;
+}
+
+function as_datetime(s) {
+    let d = new Date(s);
+    return d.getFullYear() + "-" + String(d.getMonth()+1).padStart(2, 0) + "-" + String(d.getDate()).padStart(2, 0) + " " + String(d.getHours()).padStart(2, 0) + ":" + String(d.getMinutes()).padStart(2, 0)
+}
+
+function enrich_text(tweet) {
+    let offset = 0;
+    let entities = parse_entities(tweet);
+    let text = tweet.full_text;
+    entities.forEach(function(item) {
+        text = text.substring(0, item.start+offset) + item.replacement + text.substring(item.end+offset);
+        offset += item.replacement.length - item.len;
+    });
+    return text;
+}
+
+function tweet_to_html(tweet) {
+    let full_text = enrich_text(tweet);
+    
+    let article = document.createElement("article");
+    let tweet_text = document.createElement("p");
+    let tweet_time = document.createElement("p");
+    let tweet_meta = document.createElement("p");
+    let tweet_author = document.createElement("p");
+    if (tweet.full_text.startsWith("RT @")) {
+        article.classList.add("retweet");
+        tweet_text.innerHTML = full_text.split(": ")[1];
+        tweet_author.innerText = "@" + full_text.split(": ")[0].substring(4);
+    } else {
+        article.classList.add("tweet");
+        tweet_text.innerHTML = full_text;
+        tweet_author.innerText = "Posted by @Kudusch";
+    }
+    
+    tweet_time.innerText = as_datetime(tweet.created_at);
+    tweet_time.classList.add("datetime");
+    // tweet_meta.innerText = tweet.id_str;
+    // tweet_meta.classList.add("meta");
+    article.append(tweet_text);
+    article.append(tweet_author);
+    article.append(tweet_time);
+    // article.append(tweet_meta);
+    return article;
+}
+
 function tweets_to_html(tweets) {
     let html = [];
     for (let i = 0; i < tweets.length; i++) {
         let tweet = tweets[i]["tweet"];
-        console.log(tweet);
-        let article = document.createElement("article");
-        let tweet_text = document.createElement("p");
-        let tweet_time = document.createElement("p");
-        let tweet_meta = document.createElement("p");
-        let tweet_author = document.createElement("p");
-        if (tweet["full_text"].startsWith("RT @")) {
-            article.classList.add("retweet");
-            tweet_text.innerText = tweet["full_text"].split(": ")[1];
-            tweet_author.innerText = "@" + tweet["full_text"].split(": ")[0].substring(4);
-        } else {
-            article.classList.add("tweet");
-            tweet_text.innerText = tweet["full_text"];
-            tweet_author.innerText = "@Kudusch";
-        }
-        tweet_time.innerText = tweet["created_at"];
-        tweet_meta.innerText = tweet["id_str"];
-        article.append(tweet_text);
-        article.append(tweet_time);
-        article.append(tweet_meta);
-        article.append(tweet_author);
-        html.push(article);
+        html.push(tweet_to_html(tweet));
     }
     return html;
 }
 
-document_ready(function() {
-    console.log("Ready");
-    
+function prep_tweets() {
     let tweets = window.YTD.tweets.part0;
-    
     if (sort_direction == "asc") {
         tweets.sort(function(a, b) {
             let a_time =  Date.parse(a["tweet"]["created_at"]);
@@ -70,19 +126,30 @@ document_ready(function() {
             return !tweet["tweet"]["full_text"].startsWith("@");
         });
     }
+    return tweets;
+}
+
+document_ready(function() {
+    console.log("Ready");
+    
+    let tweets = prep_tweets();
     
     const url_params = new URLSearchParams(window.location.search);
-    const cur_page = parseInt(url_params.get("p"));
-    document.getElementById("cur_page").innerHTML = cur_page;
+    let current_page = 1;
+    if (url_params.has("p")) {
+        current_page = parseInt(url_params.get("p"));
+    }
+    
+    document.getElementById("cur_page").innerHTML = current_page;
     document.getElementById("max_page").innerHTML = Math.ceil(tweets.length / tweets_per_page);
-    document.getElementById("next_page").href = "?p=" + (cur_page+1);
-    document.getElementById("prev_page").href = "?p=" + (cur_page-1);
+    document.getElementById("next_page").href = "?p=" + (current_page+1);
+    document.getElementById("prev_page").href = "?p=" + (current_page-1);
     
     document.title = "Indexing " + Object.keys(tweets).length + " Tweets by " + user_name;
-    document.getElementsByTagName("header")[0].innerHTML = "<h1>Indexing " + Object.keys(tweets).length + " Tweets by " + user_name + "</h1>";
+    document.getElementsByTagName("header")[0].children[0].innerHTML = "<h1>Indexing " + Object.keys(tweets).length + " Tweets by " + user_name + "</h1>";
     document.getElementById("info").innerHTML = "There are " + Object.keys(tweets).length + " tweets.";
-    let start_slice = (cur_page - 1 ) * tweets_per_page;
-    let end_slice = (cur_page) * tweets_per_page;
+    let start_slice = (current_page - 1 ) * tweets_per_page;
+    let end_slice = (current_page) * tweets_per_page;
     let page_tweets = tweets_to_html(tweets.slice(start_slice, end_slice));
     page_tweets.forEach(function(item) {
         document.getElementById("tweets").appendChild(item);
